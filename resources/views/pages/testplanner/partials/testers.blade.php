cf88428d9738300b863162ab6d4fea44e3f5f41e
ryan_kim <ryan.kim@mophie.com>
2016-01-21 21:33:35 -0800

    Adding and modifying library.

--- resources/views/pages/testplanner/partials/testers.blade.php
+++ resources/views/pages/testplanner/partials/testers.blade.php
@@ -18,7 +18,7 @@
                     <th class="text-center">{!! Html::image('images/firefox.png', 'Firefox', ['width' => 64, 'height' => 64]) !!}<br/>Firefox</th>
                     <th class="text-center">{!! Html::image('images/ie.png', 'IE', ['width' => 64, 'height' => 64]) !!}<br/>IE</th>
                     <th class="text-center">{!! Html::image('images/safari.png', 'Safari', ['width' => 64, 'height' => 64]) !!}<br/>Safari</th>
-                    <th class="text-center">{!! Html::image('images/apple.png', 'Apple', ['width' => 64, 'height' => 64]) !!}<br/>IOS</th>
+                    <th class="text-center">{!! Html::image('images/ios.png', 'IOS', ['width' => 64, 'height' => 64]) !!}<br/>IOS</th>
                     <th class="text-center">{!! Html::image('images/android.png', 'Android', ['width' => 64, 'height' => 64]) !!}<br/>Android</th>
                 </tr>
                 </thead>
@@ -26,12 +26,12 @@
                 @foreach($testers as $tester)
                     <tr>
                         <td>{!! $tester['first_name'] !!}</td>
-                        <td class="text-center">{!! Form::radio('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',chrome',  null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-chrome']) !!}</td>
-                        <td class="text-center">{!! Form::radio('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',firefox', null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-firefox']) !!}</td>
-                        <td class="text-center">{!! Form::radio('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',ie',      null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-ie']) !!}</td>
-                        <td class="text-center">{!! Form::radio('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',safari',  null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-safari']) !!}</td>
-                        <td class="text-center">{!! Form::radio('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',apple',   null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-apple']) !!}</td>
-                        <td class="text-center">{!! Form::radio('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',android', null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-android']) !!}</td>
+                        <td class="text-center">{!! Form::checkbox('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',chrome',  null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-chrome']) !!}</td>
+                        <td class="text-center">{!! Form::checkbox('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',firefox', null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-firefox']) !!}</td>
+                        <td class="text-center">{!! Form::checkbox('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',ie',      null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-ie']) !!}</td>
+                        <td class="text-center">{!! Form::checkbox('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',safari',  null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-safari']) !!}</td>
+                        <td class="text-center">{!! Form::checkbox('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',ios',     null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-ios']) !!}</td>
+                        <td class="text-center">{!! Form::checkbox('tester[' . $tester["id"] . ']', $tester["id"] . ',' . $tester["first_name"] . ',android', null, ['class' => 'browser-tester', 'id' => 'tester-' . $tester["id"] . '-android']) !!}</td>
                     </tr>
                 @endforeach
                 </tbody>
